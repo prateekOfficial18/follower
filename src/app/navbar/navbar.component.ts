d67be4ce6ec4ff2619d686090a7ff2a20306ec54
prateek <bhanujha021998@gmail.com>
2021-07-19 12:06:47 +0530

    github host angular

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit{
 
   constructor() { }
-
+  backgroundColor= environment.navBarBackgroundColor;
   ngOnInit() {
   }
 
